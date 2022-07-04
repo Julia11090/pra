d18baee6a247605d0cf1acf921222ce79b66d13d
Juju <gromykojulia11090@gmail.com>
2022-07-04 21:19:10 +0300

    add za

---
+++ za/защита(п)/menu.cpp
@@ -0,0 +1,35 @@
+//найти минимальный и поменять его местами с 3 элементом
+#include "Source.h"
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int key;
+	int j = 0;
+	int imin = 0;
+	int* arr = NULL;
+	do
+	{
+		key = input_key();
+		switch (key)
+		{
+		case 1:
+
+			My_Program(arr, j);
+
+			break;
+		case 2:
+			Test(arr, j);
+			break;
+		case 3:
+		{
+			cout << "выход" << endl;
+		}
+		break;
+		default:
+			cout<<"вы ввели что-то не то" << endl;
+			break;
+		}
+	} while (key != 3);
+	system("pause");
+}
